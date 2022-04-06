a06c2cc78b82c38a2e53201d30082b574b9710fe
AdrianGonzalezB <adriangonzalez@paucasesnovescifp.cat>
2022-04-06 15:59:38 +0200

    Update category_screen.dart
    ## Category Screen
    - Update the ListView.
    - Now the only draggable item it's the icon drag_handle
    - Now you can delete a item from the list forever

--- lib/screens/category_screen.dart
+++ lib/screens/category_screen.dart
@@ -11,39 +11,78 @@ class CategoryPage extends StatefulWidget {
 }
 
 class _CategoryState extends State<CategoryPage> {
-  var list = Preferences.category;
+  List<String> list = Preferences.category;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Edit Categories"),
       ),
-      floatingActionButton: AddBottom(),
+      floatingActionButton: const AddBottom(),
       body: ReorderableListView(
+        buildDefaultDragHandles: false,
         padding: const EdgeInsets.symmetric(horizontal: 0),
         children: <Widget>[
-          for (int index = 0; index < list.length; index += 1)
-            ListTile(
-              key: ValueKey(index),
-              title: Text(
-                list[index],
-                textAlign: TextAlign.left,
-                style: const TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
+          for (int index = 0; index < Preferences.category.length; index += 1)
+            Dismissible(
+              // Set a UniqueKey to resolve problems on the tree
+              key: UniqueKey(),
+              background: Container(
+                color: Colors.red,
+                child: const Align(
+                  child: Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Icon(Icons.delete_forever),
+                  ),
+                  alignment: Alignment.centerRight,
                 ),
               ),
-              leading: const Icon(Icons.drag_handle), // Draggable icon
+              child: Container(
+                key: Key('$index'),
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      width: 64,
+                      height: 64,
+                      padding: const EdgeInsets.all(8),
+                      // Contains the draggable Icon
+                      child: ReorderableDragStartListener(
+                        index: index,
+                        child: const Icon(Icons.drag_handle),
+                      ),
+                    ),
+                    // The text of the category on each Container
+                    Text(
+                      Preferences.category[index],
+                      textAlign: TextAlign.left,
+                      style: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        fontStyle: FontStyle.normal,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              onDismissed: (DismissDirection direction) {
+                // Remove the item from the data source.
+                setState(() {
+                  list.removeAt(index);
+                  Preferences.setCategory = list;
+                });
+              },
             ),
         ],
+        // User reorder the list
         onReorder: (int oldIndex, int newIndex) {
           setState(() {
+            // Gets the old index and saves the new
             if (oldIndex < newIndex) {
               newIndex -= 1;
-              final String item = Preferences.category.removeAt(oldIndex);
-              list.insert(newIndex, item);
-              Preferences.setCategory = list;
             }
+            final String item = list.removeAt(oldIndex);
+            list.insert(newIndex, item);
+            Preferences.setCategory = list;
           });
         },
       ),
